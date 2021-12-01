f815126800a0f4505ca15bb858511db92b02fdcb
Bresmar Bermudez <bresmarbermudez@gmail.com>
2021-12-01 16:28:41 -0400

    Creacion de script con python para tomar datos de entrada

---
+++ primeras_pruebas.py
@@ -0,0 +1,7 @@
+# -⁻- coding: UTF-8 -*-
+NameUser = input("Dinos tu nombre:")
+print(NameUser)
+EdadUser = input("¿Qué edad tienes?:")
+print(EdadUser)
+Resultado = "Bien! {} tienes {} años de edad".format(NameUser, EdadUser)
+print (Resultado)
